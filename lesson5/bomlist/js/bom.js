cef6dd0f1edce32cd804ebb105ad1fda2a83cc63
Carolyn Sundrud <carolyn.sundrud@gmail.com>
2021-05-19 20:13:50 -0500

    update

--- lesson5/bomlist/js/bom.js
+++ lesson5/bomlist/js/bom.js
@@ -12,7 +12,7 @@ button.addEventListener('click', function() {
 
     listChapter.appendChild(listText);
     listText.textContent = myChapter;
-    listBtn.textContent = 'X';
+    listBtn.textContent = '❌';
     listChapter.appendChild(listBtn);
     list.appendChild(listChapter);
 
