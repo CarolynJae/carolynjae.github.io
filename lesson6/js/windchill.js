7bd2fefb14fcd00fef18d267f6613e46d2e258d0
Carolyn Sundrud <carolyn.sundrud@gmail.com>
2021-05-25 18:40:34 -0500

    update

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,3 +1,13 @@
-function WindChillFactor(temperature, windspeed) {
-    return 35.74 + (0.6215 * temperature) - (35.75 * (windspeed ^ 0.16)) + (0.4275 * temperature * (windspeed ^ 0.16))  ;
-  }
+document.querySelector(".currentTemp")
+document.querySelector(".windspeed")
+
+if (currentTemp <= 50) and (windspeed < 3) {
+    function windChill(tempF, speed) {
+            
+        let f = 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, 0.16)) 
+                    + (0.4275 * tempF * Math.pow(speed, 0.16));
+        var calculation = Math.round(f)
+        return calculation;
+    } 
+}
+
