24af7a79fa54fe06ba0fadd3e1fde462c9860d7d
Carolyn Sundrud <carolyn.sundrud@gmail.com>
2021-06-06 12:40:02 -0500

    update

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,14 +1,12 @@
-let t = document.querySelector("#temperature")
-let s = document.querySelector("#windspeed")
+let t = parseFloat(document.querySelector("#temperature").innerHTML);
+let s = parseFloat(document.querySelector("#windspeed").innerHTML);
 
-if (t <= 50) && (s > 3) {
-    function windChill(t, s) {
-            
+if (t <= 50 && s > 3) {
         let f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) 
                     + (0.4275 * t * Math.pow(s, 0.16));
         var calculation = Math.round(f)
-        return calculation;
+        document.querySelector("#windChill").innerHTML = calculation + "&#8457";
     } 
+else {
+    document.querySelector("#windChill").innerHTML = "N/A";
 }
-
-//I've never been great at javascript...  
