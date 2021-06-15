6f884239e7df0f5364c366f857f95f5a14814317
Carolyn Sundrud <carolyn.sundrud@gmail.com>
2021-06-15 13:13:20 -0500

    update

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,15 +1,34 @@
 /*-- STARTER CODE SOURCE: W09 Activity: Working with JSON data and the Fetch API --*/
 
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+const requestURL =
+  "https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
 
-fetch (requestURL)
-    .then (function (response) {
-        return response.json();
-    })
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
 
-    .then (function (jsonObject) {
-        console.table(jsonObject);
-        for (let i = 0; i < prophets.length; i++ ) {
-    })
+  .then(function (jsonObject) {
+    console.table(jsonObject);
+    const prophets = jsonObject["prophets"];
+    for (let i = 0; i < prophets.length; i++) {
+        var thisProphet = prophets[i];
+        let card = document.createElement("section");
+        let prophetName = document.createElement("h2");  
+        let birthDate = document.createElement("p");
+        let birthPlace = document.createElement("p");   
+        let prophetImage = document.createElement("img");
 
-const prophets = jsonObject['prophets'];
+      prophetName.textContent = thisProphet.name + " " + thisProphet.lastname;
+      birthDate.textContent = "Date of Birth:" + " " + thisProphet.birthdate;
+      birthPlace.textContent = "Place of Birth:" + " " + thisProphet.birthplace;
+      prophetImage.setAttribute('src', thisProphet.imageurl);
+
+      card.appendChild(prophetName);
+      card.appendChild(birthDate);
+      card.appendChild(birthPlace);
+      card.appendChild(prophetImage);
+
+      document.querySelector("div.cards").appendChild(card);            
+    }
+  });
