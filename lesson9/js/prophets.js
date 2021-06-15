b13505325a06db9abad7032b2e94de97bec12114
Carolyn Sundrud <carolyn.sundrud@gmail.com>
2021-06-15 09:23:07 -0500

    update

---
+++ lesson9/js/prophets.js
@@ -0,0 +1,14 @@
+/*-- STARTER CODE SOURCE: W09 Activity: Working with JSON data and the Fetch API --*/
+
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch (requestURL)
+    .then (function (response) {
+        return response.json();
+    })
+
+    .then (function (jsonObject) {
+        console.table(jsonObject);
+    })
+
+    
