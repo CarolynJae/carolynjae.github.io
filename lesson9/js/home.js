9b4a6e77ad3fb78f9b5fe2c4e3069a620cb6e23a
Carolyn Sundrud <carolyn.sundrud@gmail.com>
2021-06-17 18:30:54 -0500

    update

--- lesson9/js/home.js
+++ lesson9/js/home.js
@@ -8,10 +8,11 @@ fetch(requestURL)
 .then(function (jsonObject) {
  //   console.table(jsonObject);
     const towns = jsonObject["towns"];
-    for (let i=0; i < towns.length; i++) {
-        if (i==0 || i==2 || i==6) {
+    for (let t=0; t <= 2; t++) {
+        if (t==0) {i=6;}
+        if (t==1) {i=0;}
+        if (t==2) {i=2;}
          
-        var thisTown = towns[i];
         let card = document.createElement("section");
         let townName = document.createElement("h2");  
         let townMotto = document.createElement("h3");
@@ -20,14 +21,13 @@ fetch(requestURL)
         let townRainfall = document.createElement("p");   
         let townImage = document.createElement("img");
 
-        townName.textContent = thisTown.name;
-        townMotto.textContent = thisTown.motto;
-        yearFounded.textContent = "Founded in" + " " + thisTown.yearFounded;
-        townPopulation.textContent = "Population:" + " " + thisTown.currentPopulation;
-        townRainfall.textContent = "Average Yearly Rainfall:" + " " + thisTown.averageRainfall;
+        townName.textContent = towns[i].name;
+        townMotto.textContent = towns[i].motto;
+        yearFounded.textContent = "Founded in" + " " + towns[i].yearFounded;
+        townPopulation.textContent = "Population:" + " " + towns[i].currentPopulation;
+        townRainfall.textContent = "Average Yearly Rainfall:" + " " + towns[i].averageRainfall;
 
-        townImage.setAttribute("src", thisTown.photo);
-        townImage.setAttribute("alt", thisTown.name);
+        
 
         card.appendChild(townName);
         card.appendChild(townMotto);
@@ -37,6 +37,21 @@ fetch(requestURL)
         card.appendChild(townImage);
 
         document.querySelector("div.cards").appendChild(card); 
+
+        if (i==6) {
+            townImage.setAttribute("src", "images/preston.jpg");
+            townImage.setAttribute("alt", towns[6].name);
+        }
+
+        if (i==0) {
+            townImage.setAttribute("src", "images/sodasprings.jpg");
+            townImage.setAttribute("alt", towns[0].name);
+        }      
+
+        if (i==2) {
+            townImage.setAttribute("src", "images/haven.jpg");
+            townImage.setAttribute("alt", towns[2].name);
+        }
     }
-}})
+})
 
