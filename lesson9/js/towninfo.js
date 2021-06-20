e069c7aa8d3485e216be118115518a24b13822d1
Carolyn Sundrud <carolyn.sundrud@gmail.com>
2021-06-20 10:47:30 -0500

    update

--- lesson9/js/towninfo.js
+++ lesson9/js/towninfo.js
@@ -1,3 +1,6 @@
+//My team (Emily, Gary, Bonnette, and I) did some problem-solving and worked through 
+//some of this code together in our team meeting on Thursday
+
 const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
 
 fetch(requestURL)
@@ -6,17 +9,19 @@ fetch(requestURL)
 })
 
 .then(function (jsonObject) {
-    console.table(jsonObject);
+//  console.table(jsonObject);
     const towns = jsonObject["towns"];
 
-//Preston          
+    //for PRESTON
+
         let card = document.createElement("section");
-        let infoDiv = document.createElement("div");
+        let townInfo = document.createElement("div");
         let townName = document.createElement("h2");  
         let townMotto = document.createElement("h4");
         let yearFounded = document.createElement("p");
         let townPopulation = document.createElement("p");
         let townRainfall = document.createElement("p"); 
+        
 
         townName.textContent = towns[6].name;
         townMotto.textContent = towns[6].motto;
@@ -24,80 +29,90 @@ fetch(requestURL)
         townPopulation.textContent = "Population:" + " " + towns[6].currentPopulation;
         townRainfall.textContent = "Average Yearly Rainfall:" + " " + towns[6].averageRainfall; 
 
-        infoDiv.appendChild(townName);
-        infoDiv.appendChild(townMotto);
-        infoDiv.appendChild(yearFounded);
-        infoDiv.appendChild(townPopulation);
-        infoDiv.appendChild(townRainfall);
+        townInfo.appendChild(townName);
+        townInfo.appendChild(townMotto);
+        townInfo.appendChild(yearFounded);
+        townInfo.appendChild(townPopulation);
+        townInfo.appendChild(townRainfall);
 
         let townImage = document.createElement("img");
         townImage.setAttribute("src", "images/preston.jpg");
         townImage.setAttribute("alt", towns[6].name);
-        card.appendChild(infoDiv);
+
+        card.appendChild(townInfo);
         card.appendChild(townImage);
+        card.setAttribute("class", "odds");
 
         document.querySelector("div.cards").appendChild(card); 
 
 
-    //Soda Springs
-         
-        let card2 = document.createElement("section");
-        let infoDiv2 = document.createElement("div");
-        let townName2 = document.createElement("h2");  
-        let townMotto2 = document.createElement("h4");
-        let yearFounded2 = document.createElement("p");
-        let townPopulation2 = document.createElement("p");
-        let townRainfall2 = document.createElement("p"); 
+    //for SODA SPRINGS
 
-        townName2.textContent = towns[0].name;
-        townMotto2.textContent = towns[0].motto;
-        yearFounded2.textContent = "Founded in" + " " + towns[0].yearFounded;
-        townPopulation2.textContent = "Population:" + " " + towns[0].currentPopulation;
-        townRainfall2.textContent = "Average Yearly Rainfall:" + " " + towns[0].averageRainfall; 
+        card = document.createElement("section");
+        townInfo = document.createElement("div");
+        townName = document.createElement("h2");  
+        townMotto = document.createElement("h4");
+        yearFounded = document.createElement("p");
+        townPopulation = document.createElement("p");
+        townRainfall = document.createElement("p"); 
+        
 
-        infoDiv2.appendChild(townName2);
-        infoDiv2.appendChild(townMotto2);
-        infoDiv2.appendChild(yearFounded2);
-        infoDiv2.appendChild(townPopulation2);
-        infoDiv2.appendChild(townRainfall2);
+        townName.textContent = towns[0].name;
+        townMotto.textContent = towns[0].motto;
+        yearFounded.textContent = "Founded in" + " " + towns[0].yearFounded;
+        townPopulation.textContent = "Population:" + " " + towns[0].currentPopulation;
+        townRainfall.textContent = "Average Yearly Rainfall:" + " " + towns[0].averageRainfall; 
 
-        let townImage2 = document.createElement("img");
-        townImage2.setAttribute("src", "images/sodasprings.jpg");
-        townImage2.setAttribute("alt", towns[0].name);
-        card2.appendChild(infoDiv2);
-        card2.appendChild(townImage2);
+        townInfo.appendChild(townName);
+        townInfo.appendChild(townMotto);
+        townInfo.appendChild(yearFounded);
+        townInfo.appendChild(townPopulation);
+        townInfo.appendChild(townRainfall);
 
-        document.querySelector("div.cards").appendChild(card2); 
+        townImage = document.createElement("img");
+        townImage.setAttribute("src", "images/sodasprings.jpg");
+        townImage.setAttribute("alt", towns[0].name);
 
-    //Fish Haven
+        card.appendChild(townInfo);
+        card.appendChild(townImage);
+        card.setAttribute("class", "even");
 
-    let card3 = document.createElement("section");
-    let infoDiv3 = document.createElement("div");
-    let townName3 = document.createElement("h2");  
-    let townMotto3 = document.createElement("h4");
-    let yearFounded3 = document.createElement("p");
-    let townPopulation3 = document.createElement("p");
-    let townRainfall3 = document.createElement("p"); 
+        document.querySelector("div.cards").appendChild(card); 
 
-    townName3.textContent = towns[2].name;
-    townMotto3.textContent = towns[2].motto;
-    yearFounded3.textContent = "Founded in" + " " + towns[2].yearFounded;
-    townPopulation3.textContent = "Population:" + " " + towns[2].currentPopulation;
-    townRainfall3.textContent = "Average Yearly Rainfall:" + " " + towns[2].averageRainfall; 
 
-    infoDiv3.appendChild(townName3);
-    infoDiv3.appendChild(townMotto3);
-    infoDiv3.appendChild(yearFounded3);
-    infoDiv3.appendChild(townPopulation3);
-    infoDiv3.appendChild(townRainfall3);
+    //for FISH HAVEN
 
-    let townImage3 = document.createElement("img");
-    townImage3.setAttribute("src", "images/haven.jpg");
-    townImage3.setAttribute("alt", towns[2].name);
-    card3.appendChild(infoDiv3);
-    card3.appendChild(townImage3);
-    
+          card = document.createElement("section");
+          townInfo = document.createElement("div");
+          townName = document.createElement("h2");  
+          townMotto = document.createElement("h4");
+          yearFounded = document.createElement("p");
+          townPopulation = document.createElement("p");
+          townRainfall = document.createElement("p"); 
+          
+  
+          townName.textContent = towns[2].name;
+          townMotto.textContent = towns[2].motto;
+          yearFounded.textContent = "Founded in" + " " + towns[2].yearFounded;
+          townPopulation.textContent = "Population:" + " " + towns[2].currentPopulation;
+          townRainfall.textContent = "Average Yearly Rainfall:" + " " + towns[2].averageRainfall; 
+           
+          townInfo.appendChild(townName);
+          townInfo.appendChild(townMotto);
+          townInfo.appendChild(yearFounded);
+          townInfo.appendChild(townPopulation);
+          townInfo.appendChild(townRainfall);
 
-    document.querySelector("div.cards").appendChild(card3); 
-
+          townInfo.setAttribute("class", "info");
+  
+          townImage = document.createElement("img");
+          townImage.setAttribute("src", "images/fishhaven.jpg");
+          townImage.setAttribute("alt", towns[2].name);
+  
+          card.appendChild(townInfo);
+          card.appendChild(townImage);
+          card.setAttribute("class", "odds");
+  
+          document.querySelector("div.cards").appendChild(card); 
+   
 })
