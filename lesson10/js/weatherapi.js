4c9148118fe2d37509d953e95322d14c186350c0
Carolyn Sundrud <carolyn.sundrud@gmail.com>
2021-06-24 12:56:57 -0500

    update

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,14 +1,19 @@
-const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=ba93ecdd2871472e91bb1e18e6fe2f14";
+const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=ba93ecdd2871472e91bb1e18e6fe2f14&units=imperial";
 
 fetch(apiURL)
   .then((response) => response.json())
 
-
   .then((jsObject) => {
-    console.log(jsObject);
-
+      console.log(jsObject);
+    console.log(jsObject.list[0].main);
    
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    document.getElementById('current-temp').textContent = jsObject.list[0].main.temp;
+
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[0].weather[0].icon + '.png'; 
+    const desc = jsObject.list[0].weather[0].description;  
+    document.getElementById('imagesrc').textContent = imagesrc;  
+    document.getElementById('icon').setAttribute('src', imagesrc);  
+    document.getElementById('icon').setAttribute('alt', desc);
 
 
   });
