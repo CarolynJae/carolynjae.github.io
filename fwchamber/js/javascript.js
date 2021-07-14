6360362fd0d4fbe2f1dc157ac852ffec661ed476
Carolyn Sundrud <carolyn.sundrud@gmail.com>
2021-07-14 11:38:57 -0500

    update

--- fwchamber/js/javascript.js
+++ fwchamber/js/javascript.js
@@ -53,3 +53,22 @@ function toggleMenu() {
     const currentDate = new Date();
 
     document.querySelector('#year').textContent = today;
+
+
+    /*Slideshow code by w3schools*/
+
+    var slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}
+      slides[slideIndex-1].style.display = "block";
+      setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
